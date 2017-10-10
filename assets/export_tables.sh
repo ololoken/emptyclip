d199b0da11fbeb270cdcd9f20040c9f057226685
jazztickets <jazztickets@gmail.com>
2017-10-09 19:24:11 -0600

    organized assets dir added export_tables script

---
+++ assets/export_tables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# cd to script dir
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$script_dir"
+
+# make temp dir
+mkdir -p temp
+
+# convert files to csv
+libreoffice --headless --convert-to csv tables/armor.ods --outdir temp/
+
+# convert from csv to tsv
+find temp/ -iname "*.csv" -exec sh -c 'f="{}";f="${f%.*}"; gawk -f scripts/csv2tsv.awk "${f}.csv" > "${f}.tsv"' \;
+
+# move files
+mv temp/*.tsv ../working/tables/
+
+# remove temp files
+rm temp/*
+rm -fd temp
