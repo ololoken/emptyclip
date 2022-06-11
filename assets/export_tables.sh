a55f5db8306e4f27c7b57b0d317187f16b7206d1
jazztickets <jazztickets@gmail.com>
2022-06-11 16:32:48 -0600

    script clean up

--- assets/export_tables.sh
+++ assets/export_tables.sh
@@ -2,7 +2,7 @@
 
 # cd to script dir
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$script_dir"
+cd "$script_dir" || exit
 
 # make temp dir
 mkdir -p temp
@@ -11,7 +11,7 @@ mkdir -p temp
 libreoffice --headless --convert-to csv tables/{armor.ods,itemdrops.ods,monsters.ods,weapons.ods} --outdir temp/
 
 # convert from csv to tsv
-find temp/ -iname "*.csv" -exec sh -c 'f="{}";f="${f%.*}"; dos2unix "${f}.csv"; gawk -f scripts/csv2tsv.awk "${f}.csv" > "${f}.tsv"' \;
+find temp/ -iname "*.csv" -exec sh -c 'f="$1";f="${f%.*}"; sed -i "s/\r//g" "${f}.csv"; sed -E -f "scripts/csv2tsv.sed" "${f}.csv" > "${f}.tsv"' shell "{}" \;
 
 # move files
 mv temp/*.tsv ../working/tables/
