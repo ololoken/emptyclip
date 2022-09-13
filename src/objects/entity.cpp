d8abdb96ed9aad86888a4d1a83e0a64fe1503241
jazztickets <jazztickets@gmail.com>
2022-09-12 22:53:45 -0600

    collision fix

--- src/objects/entity.cpp
+++ src/objects/entity.cpp
@@ -410,7 +410,7 @@ void _Entity::Move(double FrameTime) {
 	// Get a list of entities that the object is colliding with
 	glm::vec2 NewPosition = Position + MoveDirection + Velocity;
 	bool AxisAlignedPush = false;
-	if(!IsInvulnerable() && !CanFreePath()) {
+	if(!IsInvulnerable() && !CanFreePath() && !IsDying()) {
 		std::vector<_Hit> &Hits = Map->ResolveCollisionsInGrid(NewPosition, Radius, this, AxisAlignedPush, Type == PLAYER ? PLAYER_PUSH_FACTOR : ENTITY_PUSH_FACTOR);
 
 		// Resolve pushes
