c9d735fcc33093e434f4ea90669da2b4aa02574f
jazztickets <jazztickets@gmail.com>
2022-11-03 20:23:41 -0600

    update drops

--- src/objects/object.h
+++ src/objects/object.h
@@ -126,7 +126,7 @@ class _Object {
 		bool CanEquip() const { return Type == _Object::WEAPON || Type == _Object::ARMOR; }
 		bool CanMod() const { return Type == _Object::WEAPON || Type == _Object::ARMOR; }
 		bool CanLevel() const { return Type == _Object::WEAPON || Type == _Object::ARMOR || Type == _Object::MOD; }
-		bool CanUnique() const { return Type == _Object::WEAPON || Type == _Object::ARMOR || Type == _Object::MOD || Type == _Object::AMMO || Type == _Object::CONSUMABLE; }
+		bool CanUnique() const { return Type == _Object::WEAPON || Type == _Object::ARMOR || Type == _Object::MOD || Type == _Object::AMMO || Type == _Object::CONSUMABLE || Type == _Object::USABLE; }
 		bool IsAutoPickup() const { return Type == _Object::AMMO || Type == _Object::KEY || Type == _Object::CONSUMABLE; }
 		bool IsHideable() const { return Type == _Object::AMMO || Type == _Object::CONSUMABLE; }
 
