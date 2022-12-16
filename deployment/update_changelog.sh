f0b25b0b4c4d0887e533cb74c4665da6b8fd021f
jazztickets <jazztickets@gmail.com>
2022-12-16 10:31:10 -0700

    add changelog update script

---
+++ deployment/update_changelog.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+name="Empty Clip"
+file=$(ls *.xml)
+
+# get releases tag
+releases=$(awk -v name="$name" '
+function close_tags() {
+	print "\t\t\t\t</ul>"
+	print "\t\t\t</description>"
+	print "\t\t</release>"
+}
+
+BEGIN {
+	print "\t<releases>"
+	ready = 0
+}
+$0 ~ "^"name && $5 != "" {
+	ready = 1
+	print "\t\t<release version=\""$3"\" date=\""$5"\">"
+	print "\t\t\t<description>"
+	print "\t\t\t\t<ul>"
+}
+ready && $0 ~ "^-" {
+	sub(/^- /, "", $0)
+	print "\t\t\t\t\t<li>" $0 "</li>"
+}
+ready && $0 ~ "^$" {
+	close_tags()
+	ready = 0
+}
+END {
+	close_tags()
+	print "\t</releases>"
+}
+' ../CHANGELOG)
+
+# update xml file
+awk -v releases="$releases" '
+BEGIN {
+	ignore = 0
+}
+$0 ~ "^.*<releases>" {
+	ignore = 1
+	print releases
+	next
+}
+$0 ~ "^.*</releases>" {
+	ignore = 0
+	next
+}
+!ignore {
+	print
+}
+' "${file}" > "${file}_new"
+
+# update file
+mv "${file}_new" "${file}"
