f40000dff67742d983d26e6565a775ee1e8f63f4
jazztickets <jazztickets@gmail.com>
2023-01-08 14:42:04 -0700

    achievement update

--- src/constants.h
+++ src/constants.h
@@ -127,7 +127,7 @@ const  glm::vec2    PLAYER_FLASHLIGHT_SIZE               =  glm::vec2(5,10);
 //     Achievements
 const  int          ACHIEVEMENTS_VERSION                 =  2;
 const  int          ACHIEVEMENTS_LINE_EM_UP_HITS         =  30;
-const  int          ACHIEVEMENTS_BLEEDRUN_TIME           =  2*60*60;
+const  int          ACHIEVEMENTS_BLEEDRUN_TIME           =  90*60;
 const  double       ACHIEVEMENTS_MESSAGE_TIMEOUT         =  5.0;
 const  double       ACHIEVEMENTS_MESSAGE_FADETIME        =  1.0;
 //     Inventory
