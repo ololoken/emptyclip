15755c77caa0abbe867aa170d5bc651721c6f05b
jazztickets <jazztickets@gmail.com>
2024-10-23 20:50:38 -0600

    cursor fix

--- src/framework.cpp
+++ src/framework.cpp
@@ -453,7 +453,7 @@ void _Framework::LoadAssets() {
 	TextureSettings.Anisotropy = Config.Anisotropy;
 	ae::Assets.LoadTexturePack("textures/lights", TextureSettings);
 	ae::Assets.LoadTexturePack("textures/melee", TextureSettings);
-	ae::Assets.LoadCursor("game", "ui/cursor_game.png", glm::ivec2(4, 4));
+	ae::Assets.LoadCursor("game", "ui/cursor_game.png", glm::ivec2(32, 32));
 
 	// Load assets
 	ae::Assets.LoadPrograms("tables/programs.tsv");
