df2a3aea145f6d4bac8855fed047416717f41c78
jazztickets <jazztickets@gmail.com>
2025-01-17 10:31:24 -0700

    fix mingw

--- deployment/build_mingw.sh
+++ deployment/build_mingw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# bash options
+set -e
+
 # includes
 source common.inc
 
@@ -51,18 +54,43 @@ build() {
 	# remove linux only files
 	rm -f "${archive_base}"/"${project}"{,_debug*,_reldeb}
 
+	# build files to copy
+	libs=(
+		OpenAL32.dll
+		SDL2.dll
+		SDL2_image.dll
+		libbrotlicommon.dll
+		libbrotlidec.dll
+		libbz2-1.dll
+		libfreetype-6.dll
+		libgcc_s_seh-1.dll
+		libogg.dll
+		libpng16-16.dll
+		libsharpyuv.dll
+		libsqlite3-0.dll
+		libssp-0.dll
+		libstdc++-6.dll
+		libvorbis-0.dll
+		libvorbisfile-3.dll
+		libwebp.dll
+		libwebpdemux.dll
+		libwinpthread-1.dll
+		zlib1.dll
+	)
+
 	# copy dlls
-	cp /usr/$arch/bin/{OpenAL32.dll,libbz2-1.dll,libfreetype-6.dll,libssp-0.dll,libgcc_*.dll,libstdc++-6.dll,libwinpthread-1.dll,libsqlite3-0.dll,libvorbisfile-3.dll,libogg.dll,libvorbis-0.dll,SDL2.dll,SDL2_image.dll,libpng16-16.dll,zlib1.dll,libbrotlicommon.dll,libbrotlidec.dll} "${archive_base}"/
+	pushd "/usr/$arch/bin/" || exit
+	cp "${libs[@]}" "${projectdir}/deployment/${archive_base}/"
+	popd || exit
 
 	# strip exe
 	${arch}-strip "${archive_base}"/${project}.exe
 
 	# copy files
 	cp "${projectdir}"/{README,CHANGELOG} "${archive_base}"/
-	#echo "${project}.exe -editor" > "${archive_base}"/run_editor.bat
 	echo "${project}.exe -level bench" > "${archive_base}"/run_bench.bat
 	echo "${project}.exe -bench b01" > "${archive_base}"/run_b01.bat
-	#chmod +x "${archive_base}"/*.bat
+	chmod +x "${archive_base}"/*.bat
 
 	# zip
 	zip -r "${archive}" "${archive_base}"
